19591b2a3bf8ee16152c3875942541cecc562fc2
Dingus <35214896+EddieG77@users.noreply.github.com>
2018-08-10 14:16:20 -0500

    please jesus

---
+++ app/data/friends.js
@@ -0,0 +1,128 @@
+var users = [{
+    name: "LeBron",
+    photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png",
+    scores: [3,
+        
+        4,
+        3,
+        1,
+        2,
+        5,
+        2,
+        5,
+        1,
+        4
+    ]
+}, {
+    name: "Anthony",
+    photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/6583.png",
+    scores: [2,
+        
+        2,
+        4,
+        2,
+        5,
+        4,
+        3,
+        1,
+        5,
+        3
+    ]
+}, {
+    name: "Nikola",
+    photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3112335.png",
+    scores: [4, 
+
+        1,
+        2,
+        2,
+        4,
+        3,
+        5,
+        2,
+        4,
+        3
+    ]
+},
+{
+    name: "Wilson",
+    photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3194.png",
+    scores: [2, 
+
+        4,
+        3,
+        3,
+        1,
+        4,
+        2,
+        3,
+        2,
+        1
+    ]
+},
+{
+    name: "Russ",
+    photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3468.png",
+    scores: [5, 
+
+        5,
+        2,
+        2,
+        1,
+        3,
+        2,
+        1,
+        4,
+        3
+    ]
+},
+{
+    name: "Kyrie",
+    photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/6442.png",
+    scores: [4, 
+
+        3,
+        3,
+        5,
+        2,
+        4,
+        3,
+        4,
+        1,
+        5
+    ]
+},
+{
+    name: "JaVale",
+    photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3452.png",
+    scores: [1, 
+
+        5,
+        1,
+        4,
+        3,
+        1,
+        2,
+        4,
+        5,
+        2
+    ]
+},
+{
+    name: "Ricky",
+    photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4011.png",
+    scores: [2, 
+
+        3,
+        5,
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        4
+    ]
+}];
+
+module.exports = users;
